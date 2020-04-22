1786fc20a07792f4dd3906712b89f0b6500044b8
mfmoraes <melchior.alencar@gmail.com>
2020-04-22 06:06:12 -0500

    Trabalhando com caracteres

---
+++ Curso de Java parte 1/sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,16 @@
+
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		/*quanto vale o segundo?no curso da alura,errei :(,achei outra coisa,triste não mesmo,
+		 * vivendo e aprendendo,hihihi
+		 */
+		
+		System.out.println(segundo);
+	}
+}
