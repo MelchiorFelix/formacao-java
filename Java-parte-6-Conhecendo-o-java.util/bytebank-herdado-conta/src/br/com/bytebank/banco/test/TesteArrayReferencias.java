ff89be592f4c931fa74360b3f40ed1aecc0b188c
mfmoraes <melchior.alencar@gmail.com>
2020-05-12 17:10:21 -0500

    Guardando qualquer referência

--- Java-parte-6-Conhecendo-o-java.util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ Java-parte-6-Conhecendo-o-java.util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,21 +1,20 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.*;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[]  contas = new ContaCorrente[5];
+		Conta[]  contas = new Conta[5];
 		ContaCorrente cc1 = new ContaCorrente(123, 456);
-		ContaCorrente cc2 = new ContaCorrente(789, 101112);
+		ContaPoupanca cc2 = new ContaPoupanca(789, 101112);
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
 		
-		System.out.println(contas[1].getNumero());
 		
-		ContaCorrente ref = contas[1];
+		ContaCorrente ref = (ContaCorrente) contas[0];
 		
 		System.out.println(ref.getNumero());
 		
