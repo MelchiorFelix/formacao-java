78445028f013a20caa09a32ef3ed11baaea8a999
melchiorfelix <melchior.alencar@gmail.com>
2020-05-26 10:46:38 -0500

    Redirecionando o fluxo

--- Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/WebContent/listaEmpresas.jsp
+++ Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/WebContent/listaEmpresas.jsp
@@ -11,6 +11,12 @@
 	</head>
 	
 	<body>
+		
+		<c:if test="${not empty empresa}">
+			Empresa ${ empresa } cadastrada com sucesso!
+		</c:if>
+		<br/>
+		
 		Lista de empresas: <br/>
 	
 		<ul>
--- Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/src/br/com/caelum/gerenciador/servlet/NovaEmpresaServlet.java
+++ Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/src/br/com/caelum/gerenciador/servlet/NovaEmpresaServlet.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -43,10 +42,14 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		//chamar o JPS
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
 		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+		
+		response.sendRedirect("listaEmpresas");
+		
+//		//chamar o JPS
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 
 }
