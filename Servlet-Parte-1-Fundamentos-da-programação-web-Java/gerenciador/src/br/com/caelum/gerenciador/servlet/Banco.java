c495ba89d44684a4bb7e320b1cc72a6fb0a39883
melchiorfelix <melchior.alencar@gmail.com>
2020-05-27 01:37:01 -0500

    Completando o CRUD

--- Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/src/br/com/caelum/gerenciador/servlet/Banco.java
+++ Servlet-Parte-1-Fundamentos-da-programação-web-Java/gerenciador/src/br/com/caelum/gerenciador/servlet/Banco.java
@@ -1,16 +1,20 @@
 package br.com.caelum.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private  static List<Empresa> lista = new ArrayList<>();
+	private  static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);
@@ -21,8 +25,32 @@ public class Banco {
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 	}
+
+	public void remove(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+	
+	
 	
 	
 }
