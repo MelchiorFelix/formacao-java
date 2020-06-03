3e8536375801c0ffc6a8853992bc01a29307a2d2
Melchior Felix <melchior.alencar@gmail.com>
2020-06-03 00:12:13 -0500

    Cadastro de preços do produto

--- Spring-MVC-I-Criando-aplicações-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
+++ Spring-MVC-I-Criando-aplicações-web/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutoController.java
@@ -3,21 +3,26 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoDAO produtoDao;
-	
+
 	@RequestMapping("/produtos/form")
-	public String form() {
-		return "produtos/form";
+	public ModelAndView form() {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		
+		return modelAndView;
 	}
-	
+
 	@RequestMapping("/produtos")
 	public String gravar(Produto produto) {
 		produtoDao.gravar(produto);
